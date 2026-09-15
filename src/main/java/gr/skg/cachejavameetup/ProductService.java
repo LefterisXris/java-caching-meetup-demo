cbd2e4e8e68a2982265bb4bdfbbf69498651eac1
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2026-09-15 23:44:12 +0300

    ConcurrentHashMap

--- src/main/java/gr/skg/cachejavameetup/ProductService.java
+++ src/main/java/gr/skg/cachejavameetup/ProductService.java
@@ -2,25 +2,19 @@ package gr.skg.cachejavameetup;
 
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ProductService {
    private final ProductRepository productRepository;
-   // Simple Cache
-   private final Map<Long, Product> cache = new HashMap<>();
+   // Simple Cache, but thread safe
+   private final Map<Long, Product> cache = new ConcurrentHashMap<>();
 
    public ProductService(ProductRepository productRepository) {this.productRepository = productRepository;}
 
    public Product getProduct(Long id) {
-      Product cached = cache.get(id);
-      if (cached != null) {
-         return cached;
-      }
-
-      Product product = productRepository.findById(id);
-      cache.put(id, product);
-      return product;
+      // Thread Safe operation, and more elegant
+      return cache.computeIfAbsent(id, productRepository::findById);
    }
 }
