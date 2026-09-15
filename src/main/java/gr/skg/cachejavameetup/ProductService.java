0e06e738b6d8b349fba54cf3c0c8a714221212cc
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2026-09-15 23:43:08 +0300

    Simple Map: HashMap

--- src/main/java/gr/skg/cachejavameetup/ProductService.java
+++ src/main/java/gr/skg/cachejavameetup/ProductService.java
@@ -2,13 +2,25 @@ package gr.skg.cachejavameetup;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class ProductService {
    private final ProductRepository productRepository;
+   // Simple Cache
+   private final Map<Long, Product> cache = new HashMap<>();
 
    public ProductService(ProductRepository productRepository) {this.productRepository = productRepository;}
 
    public Product getProduct(Long id) {
-      return productRepository.findById(id);
+      Product cached = cache.get(id);
+      if (cached != null) {
+         return cached;
+      }
+
+      Product product = productRepository.findById(id);
+      cache.put(id, product);
+      return product;
    }
 }
